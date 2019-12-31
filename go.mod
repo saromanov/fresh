a58d6d60f54d593f29980adaf35f8f4330c6779a
Sergey <xxsmotur@gmail.com>
2019-12-31 14:44:14 +0500

    Return list with new releases

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/saromanov/fresh
 go 1.13
 
 require (
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/google/go-github/v28 v28.1.1
 	github.com/urfave/cli/v2 v2.1.1
 )
--- pkg/releases.go
+++ pkg/releases.go
@@ -4,21 +4,30 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/blang/semver"
 	"github.com/google/go-github/v28/github"
 )
 
-func NewReleases(deps []Dependency) error {
+type NewRelease struct {
+	Name string
+	Tag  string
+}
+
+// NewReleases retruns list of all new releases
+func NewReleases(deps []Dependency) ([]*NewRelease, error) {
 	client := github.NewClient(nil)
 	for _, dep := range deps {
 		releases, _, err := client.Repositories.ListReleases(context.TODO(), dep.Author, dep.Name, nil)
 		if err != nil {
-			return fmt.Errorf("unable to get release: %v", err)
+			return nil, fmt.Errorf("unable to get release: %v", err)
 		}
 
 		for _, r := range releases {
-			fmt.Println(r.GetTagName())
+			v1, _ := semver.Make(r.GetTagName())
+			v2, _ := semver.Make(dep.Tag)
+			fmt.Println(v1.Equals(v2), r.GetTagName(), dep.Tag)
 		}
 	}
 
-	return nil
+	return nil, nil
 }
