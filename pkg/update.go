b975ff61bce5d53d84b7a030d426fa1d55899fbf
Sergey <xxsmotur@gmail.com>
2020-02-15 20:31:28 +0500

    Fixing on updating release

--- pkg/update.go
+++ pkg/update.go
@@ -8,10 +8,22 @@ import (
 
 // Update provides updating of deps.
 // It might be updated specific dep or all deps
-func Update(path string) error {
+func Update(path string, releases []*NewRelease) error {
 	if path == "" {
 		return fmt.Errorf("path is not defined")
 	}
+	url := func(r *NewRelease) string {
+		return fmt.Sprintf("%s@%s", r.URL, r.Tag)
+	}
+	for _, r := range releases {
+		if err := update(url(r)); err != nil {
+			return fmt.Errorf("unable to update release: %v", err)
+		}
+	}
+	return nil
+}
+
+func update(path string) error {
 	cmd := exec.Command("go", "get", "-u", path)
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
